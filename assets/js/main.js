83e6fa8e45be5279b6e9d57368e5d6117969a1e9
m1gwan <105567098+m1gwan@users.noreply.github.com>
2023-06-02 11:51:41 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "C-ae9cMZfwU"
+const videoId = "u8nQa1cJyX8"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
